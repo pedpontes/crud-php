8cea22c5117577a485785b2ebfb20667a4a71922
unknown <pontespeter@gmail.com>
2024-08-11 13:30:08 -0300

    styles pages

--- public/head.php
+++ public/head.php
@@ -1,8 +1,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css">
         
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <?php require_once "../config/db_connection.php" ?>
--- public/header.php
+++ public/header.php
@@ -1,12 +1,10 @@
 <header>
-    <div>
-        <h1>Users Manager</h1>
-    </div>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="create.php">Adicionar item</a></li>
-                <li><a href="read.php">Listar itens</a></li>
-            </ul>
-        </nav>
+    <nav class="nav fs-1">
+        <a href="index.php" class="nav-link">Users manager</a>
+    </nav>
+    <nav class="nav">
+        <a class="nav-link" href="index.php">Home</a>
+        <a class="nav-link" href="create.php">Append users</a>
+        <a class="nav-link" href="read.php">List users</a>
+    </nav>
 </header>
